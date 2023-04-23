086926e13598949c3d55556048f228960b598dd3
CoderOfRat <1404616446@qq.com>
2023-04-23 10:06:54 +0800

    更新编辑此页的地址

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,13 +42,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/CoderOfRat/SunnyRatArea/",
+          editUrl: "https://github.com/CoderOfRat/SunnyRatArea/tree/main/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/CoderOfRat/SunnyRatArea/",
+          editUrl: "https://github.com/CoderOfRat/SunnyRatArea/tree/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
