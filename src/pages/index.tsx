7510e355fbe400cfe9c022584933756b1249b9a6
CoderOfRat <1404616446@qq.com>
2024-11-18 17:38:53 +0800

    恢复修改的内容

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,24 +3,30 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
 
 import styles from './index.module.css';
 
+import React,{useState,useEffect} from 'react';
+import TypedTextComp from './typedtext';
+import axios from 'axios';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
+  const [bgUrl,setBgUrl] = useState("");
+  useEffect(()=> {
+    axios.get("https://api.no0a.cn/api/bing/0").then(res => {
+      setBgUrl(res.data.bing.url)
+    })
+  },[]);
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero hero--primary', styles.heroBanner)} style={{backgroundImage:`url(${bgUrl})`}}>
       <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      <h1 className="hero__title">{siteConfig.title}</h1>
+      <p className="hero__subtitle"><TypedTextComp/></p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/blog/intro">
+            认识一下 - 2min ⏱️
           </Link>
         </div>
       </div>
